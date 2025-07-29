f6b3df7698e95728fd1ad90ae8c2998a4c1d710a
rahul-arakka <arakkarahul11@gmail.com>
2025-07-29 23:53:00 +0530

    RangeSumQuery.java - Arrays | All test cases passed | Approach 1

---
+++ src/main/code/Arrays/RangeSumQuery.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/range-sum-query-immutable/description/?envType=problem-list-v2&envId=array
+
+class NumArray {
+    int arr[];
+    public NumArray(int[] nums) {
+        this.arr = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for(int i=left;i<=right;i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
